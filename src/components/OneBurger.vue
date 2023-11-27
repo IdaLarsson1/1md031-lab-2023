66c7683e08448f7719a5d4b0f20c935f7b2dd6d4
Ida Larsson <ida@Idas-MacBook-Pro-3.local>
2023-11-27 20:45:36 +0100

    Klar med hela labben

--- src/components/OneBurger.vue
+++ src/components/OneBurger.vue
@@ -1,20 +1,73 @@
 <template>
-    <div>
-      {{ burger.name }} {{ burger.kCal }}
-    </div>
-  </template>
-  
-  <script>
-  export default {
-    name: 'OneBurger',
-    props: {
-      burger: Object
+  <div>
+    <h3>{{ burger.name }}</h3>
+    <img v-bind:src=burger.url style="height: 200px;">
+    <ul>
+      <li>{{ burger.kCal }} kCal</li>
+      <li v-if="burger.lactose">Innehåller <span class="allergi">laktos</span></li>
+      <li v-if="burger.gluten">Innehåller <span class="allergi">gluten</span></li>
+      <li v-if="burger.vegan">Vegansk</li>
+    </ul>
+
+    <button v-on:click="amountOrdered--">Ta bort en</button>
+    {{ amountOrdered }}
+    <button v-on:click="amountOrdered++">Lägg till en</button><br>
+    
+    <button v-on:click="addBurger">Lägg till i beställning</button>
+  </div>
+
+</template>
+
+<script>
+export default {
+  name: 'OneBurger',
+  props: {
+    burger: Object
+  },
+  data: function () {
+    return {
+      amountOrdered: 0,
+      
+    }
+  },
+  methods: {
+    addBurger: function () {   
+      this.$emit('orderedBurger', { name:   this.burger.name, 
+                                  amount: this.amountOrdered 
+                                  }
+                );
+      this.amountOrdered = 0;
     }
   }
-  </script>
-  
-  <!-- Add "scoped" attribute to limit CSS to this component only -->
-  <style scoped>
-  
-  </style>
-  
+}
+
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+.allergi {
+  font-weight: bold;
+}
+
+
+/*
+#hamburgers {
+  color: white;
+  background-color: black;
+  border: dotted white;
+}
+
+.wrapper {
+    display: grid;
+    grid-gap: 10px;
+    grid-template-columns: 33% 33% 33%;
+}
+
+.box {
+    border-radius: 5px;
+    padding: 20px;
+}
+
+*/
+</style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,22 +1,118 @@
 <template>
-  <div>
+  <div> 
     <div>
-      <h1>Burgers</h1>
-      <Burger v-for="burger in burgers"
-              v-bind:burger="burger" 
-              v-bind:key="burger.name"/>
-    </div>
-    <div id="map" v-on:click="addOrder">
-      click here
+      <header>        
+        <h1>Välkommen till Foodora 2.0</h1>
+        <img src="https://thatsup.se/content/img/place/t/u/tugg-burgers-uppsala-0.jpg">
+      </header>
+      <section id="hamburgers">
+        <div class="wrapper">
+          <Burger v-for="burger in burgers"
+                  v-bind:burger="burger" 
+                  v-bind:key="burger.name"
+                  v-on:orderedBurger="addToOrder($event)"/>
+        </div>
+      </section>
     </div>
   </div>
+
+  <section id="currentOrder">
+    <h3>Din beställning: </h3>
+    <div v-for="(number, name) in orderedBurger">
+      {{ name }}: {{ number }} st.
+    </div>
+  </section>
+
+  <section id="kundInformation">
+    <form>
+      <h3>Kundinformation</h3>
+      Skriv in din information så att beställningen kommer rätt!
+
+      <h4>Leveransinformation</h4>
+      <p>
+        <label for="Namn">Namn</label><br>
+        <input type="text" id="namn" v-model="namn" required="required" placeholder="För- och efternamn">
+      </p>
+      <p>
+        <label for="Email">E-mail</label><br>
+        <input type="email" id="mail" v-model="mail" required="required" placeholder="E-mail adress">
+      </p>
+<!--  <p>
+        <label for="Adress">Adress</label><br>
+        <input type="text" id="adress" v-model="adress" required="required" placeholder="Gata">
+      </p>
+      <p>
+        <label for="Husnummer">Hus</label><br>
+        <input type="number" id="husnummer" v-model="husnummer" required="required" placeholder="Husnummer">
+      </p>  -->
+      <p>
+        <label for="payment">Betalningsalternativ</label><br>
+        <select id="payment" v-model="payment">
+          <option>Swish</option>
+          <option>Kort</option>
+          <option>Klarna</option>
+          <option>PayPal</option>
+        </select>
+      </p>
+
+      Vart vill du få beställningen?
+      <div class="mapWrap">
+        <label>Klicka på kartan där du bor.</label>
+        <div id="map" v-on:click="setLocation">
+          <div v-bind:style="{left: this.location.x + 'px', 
+                              top: this.location.y + 'px'}">
+            <span>T</span>
+          </div>
+        </div>
+      </div>
+      <p>
+        <label for="Gender">Kön</label><br>
+        <input type="radio" id="man" value="man" v-model="gender" required="required">
+        <label for="Man">Man</label><br>
+        <input type="radio" id="kvinna" value="kvinna" v-model="gender" required="required">
+        <label for="Kvinna">Kvinna</label><br>
+        <input type="radio" id="Vill ej ange" value="vill ej ange" v-model="gender" required="required" checked="checked">
+        <label for="Vill ej ange">Annat/Vill ej ange</label><br>
+      </p> 
+    </form>             
+  </section>
+  
+  <section id="beställ">
+    <button type="submit" v-on:click="beställ">
+      <img src="https://cdn-icons-png.flaticon.com/512/3075/3075977.png" style="width: 20px;">
+      Skicka beställning
+    </button>
+  </section>
+
+  <footer>
+    <hr>
+    &copy; 2023 Ida Larsson
+  </footer>
 </template>
 
 <script>
 import Burger from '../components/OneBurger.vue'
 import io from 'socket.io-client'
+import menu from '../assets/menu.json'
 
+console.log(menu); 
 const socket = io();
+
+/*function MenuItem(name, url, kCal, l, g, v) {
+  this.name = name;
+  this.url = url;
+  this.kCal = kCal;
+  this.lactose = l;
+  this.gluten = g;
+  this.vegan = v;
+} 
+
+const minHamburgare = [
+  new MenuItem("Den nyttiga", "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQyojvzAQkWU2Z03zMI5nAIY-5xH0wn0nvXLQ&usqp=CAU", "300", false, true, true),
+  new MenuItem("Den höga", "https://www.shakentogetherlife.com/wp-content/uploads/2022/10/hamburger-baked-in-oven.jpg", "500", true, false, false),
+  new MenuItem("Smashburgaren", "https://media-cdn.tripadvisor.com/media/photo-s/25/18/0e/8a/original-smash-burger.jpg", "1000", true, true, false)
+]; */
+/* console.log(minHamburgare); */
 
 export default {
   name: 'HomeView',
@@ -25,23 +121,55 @@ export default {
   },
   data: function () {
     return {
-      burgers: [ {name: "small burger", kCal: 250},
-                 {name: "standard burger", kCal: 450},
-                 {name: "large burger", kCal: 850}
-               ]
+      burgers: menu,
+      namn:'',
+      mail:'',
+      payment:'',
+      gender:'',
+      orderedBurger: {},
+      location: {x:0, y:0}
     }
   },
   methods: {
     getOrderNumber: function () {
       return Math.floor(Math.random()*100000);
     },
-    addOrder: function (event) {
+  /*  addOrder: function (event) {
       var offset = {x: event.currentTarget.getBoundingClientRect().left,
                     y: event.currentTarget.getBoundingClientRect().top};
-      socket.emit("addOrder", { orderId: this.getOrderNumber(),
+      this.location = {x: event.clientX - 10 - offset.x,
+                       y: event.clientY - 10 - offset.y},
+       socket.emit("addOrder", { orderId: this.getOrderNumber(),
                                 details: { x: event.clientX - 10 - offset.x,
                                            y: event.clientY - 10 - offset.y },
                                 orderItems: ["Beans", "Curry"]
+                              }
+                 ); 
+    }, */
+    setLocation: function (event) {
+      var offset = {x: event.currentTarget.getBoundingClientRect().left,
+                    y: event.currentTarget.getBoundingClientRect().top};
+      this.location = {x: event.clientX - 10 - offset.x,
+                       y: event.clientY - 10 - offset.y}
+    },
+
+    addToOrder: function (event) {
+      if (this.orderedBurger[event.name] == null) {
+        this.orderedBurger[event.name] = event.amount;
+      }  
+      else {
+        this.orderedBurger[event.name] += event.amount;
+      }
+    },
+    beställ: function (event) {
+      this.kundInformation = {name: this.namn, mail: this.mail, payment: this.payment, gender: this.gender},
+      console.log(this.kundInformation),
+      console.log(this.orderedBurger),
+      socket.emit("addOrder", { orderId: this.getOrderNumber(),
+                                details: { x: this.location.x ,
+                                           y: this.location.y },
+                                orderItems: this.orderedBurger,
+                                customer: this.kundInformation                             
                               }
                  );
     }
@@ -50,9 +178,95 @@ export default {
 </script>
 
 <style>
-  #map {
-    width: 300px;
-    height: 300px;
-    background-color: red;
-  }
+@import 'https://fonts.googleapis.com/css?family=Pacifico|Dosis';
+
+body {
+  font-family: Kanit, sans-serif;
+}
+header {
+  margin: 10px;
+  height: 250px;
+  overflow: hidden;
+}
+header > img {
+  opacity: 0.6;
+  width: 100%;
+  height: auto;
+}
+header > h1 {
+  position: absolute;
+  padding: 40px 40px;
+}
+section {
+  margin: 10px;
+  padding: 10px;
+}
+button {
+  margin: 10px;
+  background-color: rgb(198, 141, 225);
+  border: 1px dotted gainsboro;
+  border-radius: 20px;
+}
+button:hover {
+  background-color: plum;
+  cursor: pointer;
+}
+.allergi {
+  font-weight: bold;
+}
+#hamburgers {
+  padding: 20px;
+  color: white;
+  background-color: black;
+  border: dotted white;
+}
+.wrapper {
+  display: grid;
+  grid-gap: 10px;
+  grid-template-columns: 33% 33% 33%;
+}
+.box {
+  border-radius: 5px;
+  padding: 20px;
+}
+#beställ > button {
+  width: 270px;
+  height: 50px;
+  font-size: x-large;
+  font-weight: bold;
+}
+#currentOrder > h3 {
+  margin-bottom: 10px;
+}
+#currentOrder {
+  padding: 20px;
+  border: dotted black;
+  background-color: rgb(198, 141, 225);
+}
+#kundInformation {
+  padding: 20px;
+  border: dotted black;
+  /*padding-top: 0px;*/
+  padding-bottom: 0px;
+}
+#map {
+  width: 1920px;
+  height: 1078px;
+  background: url("../../public/img/polacks.jpg");
+  cursor: crosshair;
+}
+#map div {
+  position: relative;
+  background: black;
+  color: white;
+  border-radius: 10px;
+  width:20px;
+  height:20px;
+  text-align: center;
+}
+.mapWrap {
+  width: 500px;
+  height: 400px;
+  overflow: scroll;
+}
 </style>
